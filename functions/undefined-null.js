0d90007a9cf4af85b537e5d15367fed43ceb3067
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-17 19:11:42 +0000

    started work on undefined

---
+++ functions/undefined-null.js
@@ -0,0 +1,24 @@
+// undefined for variable
+
+let name;
+name = 'Ashley'
+
+if (name === undefined) {
+    console.log('Please provide a name!')
+} else {
+    console.log(name)
+}
+
+// undefined for function
+// undefined as function return default value 
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square();
+console.log(result);
+
+//
+let age = 31
+age = undefined
+console.log(age);
