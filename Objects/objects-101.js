519f6faad69b190997a683e065f611a80b849134
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-18 20:00:39 +0000

    started work on objects, completed first challenge

---
+++ Objects/objects-101.js
@@ -0,0 +1,29 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// challenge area
+// create a person object, name, age and location 
+// make a string that says person is age and lives in location
+// then increase age by one and print message again
+
+let myPerson = {
+    name: 'Ashley',
+    age: 31,
+    location: 'Manchester'
+}
+
+console.log(`${myPerson.name} is ${myPerson.age} years old and lives in ${myPerson.location}`)
+
+myPerson.age++
+
+console.log(`${myPerson.name} is ${myPerson.age} years old and lives in ${myPerson.location}`)
