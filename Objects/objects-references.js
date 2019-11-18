60fa6f078c35f845f26b559b3e7d3a2b4591e1f2
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-18 21:11:55 +0000

    completed object references challenge

---
+++ Objects/objects-references.js
@@ -0,0 +1,40 @@
+// challenge area 
+// addIncome, (which account)
+// reset account (make the expenses and income to zero, (which account)) - static zero
+// getAccountSummary - prints current balance 
+// for example, ashley has 900, 1000 in income and 100 in expenses
+
+let myAccount = {
+    name: 'Ashley',
+    expenses: 0,
+    income: 0
+}
+
+let addIncome = function (account, amount) {
+    account.income = account.income + amount
+}
+
+let addExpense = function (account, amount) {
+    account.expenses = account.expenses + amount
+}
+
+let getAccountSummary = function (account) {
+    let total = (account.income - account.expenses)
+    return `The account for ${account.name} has a total of ${total} as it has ${account.income} for income and ${account.expenses} for expenses`
+}
+
+let resetAccount = function (account) {
+    account.income = 0,
+        account.expenses = 0
+}
+
+addIncome(myAccount, 2000)
+console.log(myAccount)
+addExpense(myAccount, 100)
+console.log(myAccount)
+getAccountSummary(myAccount)
+console.log(myAccount)
+resetAccount(myAccount)
+console.log(myAccount)
+addIncome(myAccount, 900)
+console.log(myAccount)
