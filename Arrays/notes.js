8e56d8b0f64f1446ec12fffe43dbcb1f7a781978
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-19 17:06:14 +0000

    started work on array challenges

---
+++ Arrays/notes.js
@@ -0,0 +1,7 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+
+console.log(notes.length) // 3 items returns 3
+// bracket notation here
+console.log(notes[0]) //accessing an item by its index
+console.log(notes[notes.length - 1]) // returns the last item
---
+++ Arrays/todo.js
@@ -0,0 +1,8 @@
+//challenge area
+// create an area of todos, five different todos
+// print a message that includes the length (how many todos, where x is the actual length)
+// print the first and second to last items --> todo: actual todo text 
+
+const toDoList = ['Learn React', 'Start using Nightwatch', 'Implement TDD', 'Learn more about Javascript', 'Console log errors']
+
+console.log(`You have ${toDoList.length} todos, the first one is ${toDoList[0]} and the second to last one is ${toDoList[toDoList.length - 2]}`)
